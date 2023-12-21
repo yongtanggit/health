163f813e2cce5c80d875bf00b66d12167ed6a688
Yong Tang <yong@yongtang.tech>
2023-12-21 13:05:39 -0800

    first commit

---
+++ ydl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# url = "https://www.youtube.com/watch?v=myv7yydtCKc"
+# output_dir = ~/coding/testing/health
+
+yt-dlp https://www.youtube.com/watch?v=myv7yydtCKc -o "~/coding/testing/health/fat"
+
